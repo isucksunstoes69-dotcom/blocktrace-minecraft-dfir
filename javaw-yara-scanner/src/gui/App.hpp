f170ba38e96cf2bf163ceb67e8b7391d3de17064
isucksunstoes69-dotcom <isucksunstoes69@gmail.com>
2026-08-20 19:27:14 +0200

    Add Javaw-style scan loading screen

--- javaw-yara-scanner/src/gui/App.hpp
+++ javaw-yara-scanner/src/gui/App.hpp
@@ -3,6 +3,9 @@
 #include "gui/Starfield.hpp"
 #include "scanner/YaraRuleScanner.hpp"
 #include <array>
+#include <atomic>
+#include <chrono>
+#include <thread>
 #include <string>
 
 struct GLFWwindow;
@@ -13,7 +16,7 @@ public:
     JavaApp(); ~JavaApp();
     bool Initialize(); void Run(); void Shutdown();
 private:
-    void DrawTitleBar(); void DrawMainWindow(float dt); void BrowseForRuleFile(); void ScanRuleFile();
+    void DrawTitleBar(); void DrawMainWindow(float dt); void DrawLoadingWindow(float dt); void BrowseForRuleFile(); void ScanRuleFile(); void FinishScan();
     GLFWwindow* m_window{};
     Starfield m_starfield;
     scanner::ScanResult m_result;
@@ -21,5 +24,9 @@ private:
     std::string m_status{"Choose a readable rule file (.txt, .md, .json, .yml, .yaml)."};
     std::string m_reportPath;
     bool m_hasResult{false};
+    std::thread m_scanThread;
+    std::atomic_bool m_scanning{false};
+    std::atomic_bool m_scanReady{false};
+    std::chrono::steady_clock::time_point m_scanStarted{};
 };
 }
